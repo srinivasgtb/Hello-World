ae91d690ad9adc1b12f6a62f3583e74adec5a638
srinivas <srinivas.rama001@gmail.com>
2021-05-17 21:03:05 +0530

    First repository successfully created

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,22 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void nativeandroid()
+	
+	{
+		System.out.println("Nativeandriod");
+	}
+	
+	@Test
+	public void iosapps()
+	{
+		System.out.println("iosapps");
+			
+	}
+
+
+}
---
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,24 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	
+	
+	@Test
+	public void postjira()
+	
+	{
+		System.out.println("postjira");
+		
+	}
+	
+	@Test
+	public void deletetwitter()
+	{
+		System.out.println("deletetwitter");
+		
+	}
+
+}
