92402b1dbd8eaf984c1d95ec3e5e65b50e10de27
SRINIVAS <srinivas.rama001@gmail.com>
2021-05-18 22:13:32 +0530

    American Guy

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -18,7 +18,17 @@ public class RESTAPITest {
 		System.out.println("postjira5");
 		
 	}
+public void ptjira()
 	
+	{
+		System.out.println("postjira");
+		System.out.println("postjira1");
+		System.out.println("postjira2");
+		System.out.println("postjira3");
+		System.out.println("postjira4");
+		System.out.println("postjira5");
+		
+	}
 	@Test
 	public void deletetwitter()
 	{
